7fe51b61af861772ac9dd5a863e441e50a576375
Elohai Kajal <kl.negi03@gmail.com>
2018-09-02 18:21:59 +0530

    feat: add send message funtionality

--- src/containers/ChatWindow.js
+++ src/containers/ChatWindow.js
@@ -5,14 +5,13 @@ import Header from '../components/Header';
 import Chats from '../components/Chats';
 import './ChatWindow.css';
 import MessageInput from './MessageInput';
-import { state } from '../static-data';
-
-const { typing } = state;
 
 const ChatWindow = ({ activeUserId }) => {
   const state = store.getState();
   const activeUser = state.contacts[activeUserId];
   const activeMsgs = state.messages[activeUserId];
+  const { typing } = state;
+
   return (
     <div className="ChatWindow">
       <Header user={activeUser} />
--- src/containers/MessageInput.js
+++ src/containers/MessageInput.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import store from '../store';
-import { setTypingValue } from '../actions';
+import { setTypingValue, sendMessage } from '../actions';
 import './MessageInput.css';
 
 const MessageInput = ({ value }) => {
   const handleChange = (e) => {
     store.dispatch(setTypingValue(e.target.value));
+  };
+
+  const state = store.getState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { typing, activeUserId } = state;
+    store.dispatch(sendMessage(typing, activeUserId));
   };
 
   return (
